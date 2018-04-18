6433abe966aa9bb672123b446485b123d4d18a1c
oliver.nina <olivernina@gmail.com>
2018-04-18 04:44:38 -0400

    attention

--- models/crnn.py
+++ models/crnn.py
@@ -1,7 +1,6 @@
 import torch.nn as nn
 import torch as torch
 
-
 class BidirectionalLSTM(nn.Module):
 
     def __init__(self, nIn, nHidden, nOut):
@@ -85,3 +84,131 @@ class CRNN(nn.Module):
         output = self.rnn(conv)
 
         return output
+
+
+use_cuda = torch.cuda.is_available()
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+class EncoderRNN(nn.Module):
+    def __init__(self,nc,nh,leakyRelu=False):# imgH, nc, nclass, nh, n_rnn=2, leakyRelu=False):
+        super(EncoderRNN, self).__init__()
+        # self.hidden_size = hidden_size
+
+        self.hidden_size = nh
+        # self.output_size = nc
+        self.max_length = 100
+        self.attn = nn.Linear(self.hidden_size , self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+
+        ks = [3, 3, 3, 3, 3, 3, 2]
+        ps = [1, 1, 1, 1, 1, 1, 0]
+        ss = [1, 1, 1, 1, 1, 1, 1]
+        nm = [64, 128, 256, 256, 512, 512, 512]  # size of each layer
+
+        cnn = nn.Sequential()
+
+        def convRelu(i, batchNormalization=False):
+            nIn = nc if i == 0 else nm[i - 1]
+            nOut = nm[i]
+            cnn.add_module('conv{0}'.format(i),
+                           nn.Conv2d(nIn, nOut, ks[i], ss[i], ps[i]))
+            if batchNormalization:
+                cnn.add_module('batchnorm{0}'.format(i), nn.BatchNorm2d(nOut))  # reduces overfitting
+
+            # always add a Relu as well (rectifies all negative values to 0, which trains it faster, helps to alleviate the vanishing gradient problem which is the issue where the lower layers of the network train very slowly because the gradient decreases exponentially through the layers")
+            if leakyRelu:
+                cnn.add_module('relu{0}'.format(i),
+                               nn.LeakyReLU(0.2, inplace=True))
+            else:
+                cnn.add_module('relu{0}'.format(i), nn.ReLU(True))
+
+        convRelu(0)
+        cnn.add_module('pooling{0}'.format(0),
+                       nn.MaxPool2d(2, 2))  # 64x16x64 pooling is downsampling, with a "stride" overlap
+        convRelu(1)
+        cnn.add_module('pooling{0}'.format(1), nn.MaxPool2d(2, 2))  # 128x8x32
+        convRelu(2, True)
+        convRelu(3)
+        cnn.add_module('pooling{0}'.format(2),
+                       nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 256x4x16
+        convRelu(4, True)
+        convRelu(5)
+        cnn.add_module('pooling{0}'.format(3),
+                       nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 512x2x16
+        convRelu(6, True)  # 512x1x16
+
+        self.cnn = cnn
+
+    def forward(self, input):
+        # embedded = self.embedding(input).view(1, 1, -1)
+        # output = embedded
+        # # output, hidden = self.gru(output, hidden)
+        # return output, hidden
+
+        # conv features (cnn)
+        conv = self.cnn(input)
+        b, c, h, w = conv.size()
+
+        # Max Pool Height Dimension for variable height inputs
+        f_pool = nn.MaxPool2d((h, 1), (1, 1))
+        conv = f_pool(conv)
+        b, c, h, w = conv.size()
+
+        assert h == 1, "the height of conv must be 1"
+        conv = conv.squeeze(2)
+        conv = conv.permute(2, 0, 1)  # [w, b, c]
+
+        # # rnn features
+        # output = self.rnn(conv)
+        output = conv
+
+        return output
+
+    def initHidden(self):
+        result = Variable(torch.zeros(1, 1, self.hidden_size))
+        if use_cuda:
+            return result.cuda()
+        else:
+            return result
+
+MAX_LENGTH = 100
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, nh, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = nh
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(512 + self.hidden_size, self.hidden_size) #units from enconder + attn_applied
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
+
+        attn_weights = F.softmax(
+            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
+
+    def initHidden(self):
+        result = Variable(torch.zeros(1, 1, self.hidden_size))
+        if use_cuda:
+            return result.cuda()
+        else:
+            return result
