4d7650925142f2b1e9deab37fd1a67bebfc927bf
oliver.nina <oliver.nina@wright.edu>
2018-05-08 16:27:43 -0400

    attention

--- nbs/finetune_jobs.py
+++ nbs/finetune_jobs.py
@@ -23,7 +23,7 @@ def create_qsub_file(name, bash_command):
     f.write("#PBS -j oe\n")
     f.write("source /home/oliver/.personal.bashrc\n")
     f.write("cd /p/home/oliver/projects/nephi\n")
-    f.write(bash_command)
+    f.write(bash_command+"\n")
     f.write("exit\n")
     f.close()
 
@@ -49,26 +49,36 @@ def main(argv):
     dirs = [os.path.basename(f).partition(".lst")[0] for f in spec_lists_files]
 
     lmdb_database_base = "data/lmdb_ICFHR_bin/specific_data_each_doc/"
-    pre_model = "results_icfhr_aug/attention+ctc/netCNN_24_2982.pth"
+    pre_model = "results_icfhr_aug/ctc_pretrain/netCNN_28_2982.pth"
+    # pre_model = "results_icfhr_aug/attention+ctc/netCNN_24_2982.pth"
 
     
 
     idx = int(argv[2])
     for num in set([d.partition("_train_")[0] for d in dirs]):
         for s in ["1", "4", "16"]:
+            # bash_command = ' '.join(["python crnn_main.py", "--trainroot", os.path.join(lmdb_database_base, num + "_train_" + s),
+                               # "--valroot", os.path.join(lmdb_database_base, num + "_train_" + "8"),
+                               # "--dataset ICFHR --cuda --lr 0.0001 --displayInterval 4 --valEpoch 1 --saveEpoch 1 --workers 3",
+                               # "--niter 60 --keep_ratio --imgH 60 --imgW 240 --batchSize 4",
+                               # "--transform --rescale --rescale_dim 3 --grid_distort",
+                               # "--model attention+ctc --plot",
+                               # "--rdir", "experiments/7May_finetuning/expr_" + "ICFHR_7May_finetuning_attention+ctc_" + num + "_train_" + s,
+                               # "--pre_model",  pre_model, ">",
+                               # "logs/icfhr/finetune/log_ICFHR_7May_finetuning_attention+ctc_" + num + "_train_" + s + ".txt"])
             bash_command = ' '.join(["python crnn_main.py", "--trainroot", os.path.join(lmdb_database_base, num + "_train_" + s),
                                "--valroot", os.path.join(lmdb_database_base, num + "_train_" + "8"),
                                "--dataset ICFHR --cuda --lr 0.0001 --displayInterval 4 --valEpoch 1 --saveEpoch 1 --workers 3",
                                "--niter 60 --keep_ratio --imgH 60 --imgW 240 --batchSize 4",
                                "--transform --rescale --rescale_dim 3 --grid_distort",
-                               "--model attention+ctc --plot",
-                               "--rdir", "experiments/7May_finetuning/expr_" + "ICFHR_7May_finetuning_attention+ctc_" + num + "_train_" + s,
+                               "--model ctc_pretrain --plot",
+                               "--rdir", "experiments/7May_finetuning/expr_" + "ICFHR_7May_finetuning_ctc_pretrain_" + num + "_train_" + s,
                                "--pre_model",  pre_model, ">",
-                               "logs/icfhr/finetune/log_ICFHR_7May_finetuning_attention+ctc_" + num + "_train_" + s + ".txt"])
+                               "logs/icfhr/finetune/log_ICFHR_7May_finetuning_ctc_pretrain_" + num + "_train_" + s + ".txt"])
             if DEBUG:
                 print(bash_command)
                 filename = create_qsub_file(str(idx), bash_command)
-                submit_qsub(filename)
+                # submit_qsub(filename)
                 sys.exit(0)
 
             else:
