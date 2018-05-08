854337041ea76b32642162826f56057649bb4dc0
oliver.nina <olivernina@gmail.com>
2018-05-08 14:12:10 -0400

    attention+ctc

---
+++ nbs/get_predictions.py
@@ -0,0 +1,106 @@
+__author__ = 'onina'
+
+# execute this script from the home directory i.e. python nps/finetune.py 0 0 
+
+import os
+import sys
+from time import gmtime, strftime
+import os
+import sys
+from glob import glob
+import shutil
+import subprocess
+from nbs.choose_models import find_models
+
+def create_qsub_file(name, bash_command):
+    filename =  "pbs/icfhr/test/"+name + '.pbs'
+    f = open(filename, 'w')
+    f.write("#!/bin/bash \n")
+    f.write("#PBS -A AFSNW35489ANO\n")
+    f.write("#PBS -l select=1:ncpus=28:ompthreads=8:ngpus=2\n")
+    f.write("#PBS -q GPU_RD\n")
+    f.write("#PBS -l walltime=05:00:00\n")
+    f.write("#PBS -N " + name + "\n")
+    f.write("#PBS -j oe\n")
+    f.write("source /home/oliver/.personal.bashrc\n")
+    f.write("cd /p/home/oliver/projects/nephi\n")
+    f.write(bash_command)
+    f.write("exit\n")
+    f.close()
+
+
+    return filename
+
+def submit_qsub(filename):
+    os.system("cd pbs/icfhr/test")
+    command = 'qsub ' + filename
+    print(command)
+    print(filename)
+    os.system(command)
+
+def main(argv):
+
+    DEBUG = int(argv[1])
+    res_dir = argv[2]
+    model = argv[3]
+
+
+    lmdb_database_base = "data/lmdb_ICFHR_bin/specific_data_each_doc/"
+    spec_tr_lists_dir = "data/datasets/read_ICFHR/specific_data_train_list/"
+
+    spec_lists_files = glob(os.path.join(spec_tr_lists_dir, "*"))
+    
+    dirs = [os.path.basename(f).partition(".lst")[0] for f in spec_lists_files]
+
+    lmdb_database_base = "data/lmdb_ICFHR_bin/specific_data_each_doc/"
+    # pre_model = "results_icfhr_aug/attention+ctc/netCNN_24_2982.pth"
+
+    tuned_models = find_models(res_dir,model)
+
+    job_id = 0
+    task_idx=0
+    for num in set([d.partition("_train_")[0] for d in dirs]):
+        for s in ["0", "1", "16", "4"]:
+            if s == "0":
+                bash_command = ' '.join(["python crnn_main.py", "--trainroot", os.path.join(lmdb_database_base, num),
+                                   "--valroot", os.path.join(lmdb_database_base, num),
+                                   "--dataset ICFHR --cuda --lr 0.0001 --displayInterval 4 --valEpoch 1 --saveEpoch 1 --workers 3",
+                                   "--niter 60 --keep_ratio --imgH 60 --imgW 240 --batchSize 4",
+                                   "--transform --rescale --rescale_dim 3 --grid_distort",
+                                   "--pre_model",
+                                   "best_models/"+model+"/netCNN_24_2982.pth",
+                                   "--test_icfhr --test_file",
+                                   os.path.join("test_results/7May_firstfinetune_submission", num + "_" + s + ".txt"),
+                                   ">",
+                                   "logs/test/"+model +"/log_7May_test_results_" + num + "_" + s + ".txt"])
+
+            else:
+                bash_command = ' '.join(["python crnn_main.py", "--trainroot", os.path.join(lmdb_database_base, num),
+                                   "--valroot", os.path.join(lmdb_database_base, num),
+                                   "--dataset ICFHR --cuda --lr 0.0001 --displayInterval 4 --valEpoch 1 --saveEpoch 1 --workers 3",
+                                   "--niter 60 --keep_ratio --imgH 60 --imgW 240 --batchSize 4",
+                                   "--transform --rescale --rescale_dim 3 --grid_distort",
+                                   "--pre_model", tuned_models[task_idx],
+                                   "--test_icfhr --test_file",
+                                   os.path.join("test_results/7May_firstfinetune_submission", num + "_" + s + ".txt"),
+                                   ">",
+                                    "logs/test/"+model +"/log_7May_test_results_" + num + "_" + s + ".txt"])
+                task_idx+=1
+
+
+
+            if DEBUG:
+                print(bash_command)
+                filename = create_qsub_file(str(job_id), bash_command)
+                sys.exit(0)
+
+            else:
+                filename = create_qsub_file(str(job_id),bash_command)
+                submit_qsub(filename)
+
+            job_id+=1
+
+if __name__=="__main__":
+
+
+   main(sys.argv)
